de97b774a0ee624774f70c058c5e23e83ebed2fb
mud <mudassirmirza@users.noreply.github.com>
2022-01-15 02:21:27 -0500

    added zoom functionality to images

--- src/components/image-card/Card.js
+++ src/components/image-card/Card.js
@@ -2,25 +2,34 @@ import { useState, useEffect } from "react";
 import { getImageByDate } from "../../services/Api";
 import "./Card.scss";
 
-export const Card = (props) => {
+export const Card = ({ date }) => {
   const [imageInfo, setImageInfo] = useState("");
   const [currentUrl, setCurrentUrl] = useState("");
   const [likeBtnText, setLikeBtnText] = useState("Like");
   const [imageClass, setImageClass] = useState("card__image");
+  const [bodyStyle, setBodyStyle] = useState("");
+  const [divClass, setDivClass] = useState("card__image-wrapper");
 
   useEffect(() => {
-    getImageByDate(props.date).then((imageData) => {
+    getImageByDate(date).then((imageData) => {
       setImageInfo(imageData);
       setCurrentUrl(imageData.url);
     });
-  }, [props]);
+  }, [date]);
 
-  const handleQualityButton = () => {
+  useEffect(() => {}, [currentUrl]);
+
+  const handleImageClick = () => {
     if (currentUrl === imageInfo.url) {
-      setImageClass("card__image--clicked");
+      setBodyStyle(window.getComputedStyle(document.body).overflow);
+      document.body.style.overflow = "hidden";
       setCurrentUrl(imageInfo.hdurl);
+      setImageClass(imageClass + "--clicked");
+      setDivClass("card__image-wrapper--clicked");
     } else {
+      document.body.style.overflow = bodyStyle;
       setImageClass("card__image");
+      setDivClass("");
       setCurrentUrl(imageInfo.url);
     }
   };
@@ -36,11 +45,13 @@ export const Card = (props) => {
   return (
     <>
       <article className="card">
-        <img
-          className={imageClass}
-          src={currentUrl}
-          onClick={handleQualityButton}
-        />
+        <div className={divClass}>
+          <img
+            className={imageClass}
+            src={currentUrl}
+            onClick={handleImageClick}
+          />
+        </div>
         <button className="card__like-button" onClick={handleLikeButton}>
           {likeBtnText}
         </button>
