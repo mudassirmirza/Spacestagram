bafb5216334e9db284690e88c05fb3d8e3a67561
mud <mudassirmirza@users.noreply.github.com>
2022-01-15 19:24:18 -0500

    added iterative rendering for cards

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import "./App.scss";
 import { Header } from "./components/header/header.js";
 import { Card } from "./components/image-card/Card.js";
 function App() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [cardDates, setCardDates] = useState([]);
+  const [numLoaded, setNumLoaded] = useState(0);
+  const [numImages, setNumImages] = useState(2);
+
+  useEffect(() => {
+    setCardDates([
+      ...cardDates,
+      `${currentDate.getFullYear()}-${
+        currentDate.getMonth() + 1
+      }-${currentDate.getDate()}`,
+    ]);
+    setNumLoaded((currNum) => currNum + 1);
+  }, [currentDate]);
+
+  useEffect(() => {
+    if (numLoaded < numImages) {
+      let tempDate = currentDate.getTime();
+      tempDate -= 86400000;
+      setCurrentDate(new Date(tempDate));
+      console.log("tempdate: " + currentDate.getDate());
+    }
+  }, [numLoaded]);
+
   return (
     <div className="app">
       <Header />
       <div className="app__card-list">
-        <Card date={"2022-01-13"} />
+        {cardDates?.map((givenDate) => (
+          <Card date={givenDate} key={"card-key-" + givenDate} />
+        ))}
       </div>
     </div>
   );
